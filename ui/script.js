a33ec261b1203eb0dff88c65cfefae424711b84b
patschwendeman <85462423+patschwendeman@users.noreply.github.com>
2024-04-26 16:15:05 +0200

    Added last Imgs

--- ui/script.js
+++ ui/script.js
@@ -27,7 +27,7 @@ function send(){
     const host = "Hackerman";
     const password = "Alles_gute_zum_30";
 
-    const imgIds = ["img_1", "img_2", "img_3", "img_4", "img_5", "img_6", "img_7", "img_8", "img_9", "img_10"];
+    const imgIds = ["img_1", "img_2", "img_3", "img_4", "img_5", "img_6", "img_7", "img_8", "img_9", "img_10", "img_11", "img_12", "img_13", "img_14", "img_15", "img_16", "img_17", "img_18", "img_19", "img_20", "img_21", "img_22"];
 
 
     if(host === inputHost && password === inputPassword) {
